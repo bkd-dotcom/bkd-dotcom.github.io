d2f6c8fa9d9b3293f0f0a8d2d5fb078dc3b968ff
Binay <150876063+bkd-dotcom@users.noreply.github.com>
2025-01-01 22:01:11 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from './components/Header';
 import { Hero } from './components/Hero';
 import { About } from './components/About';
 import { Skills } from './components/Skills';
+import { Experience } from './components/Experience';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
 import { Background } from './components/Background';
@@ -17,6 +18,7 @@ export default function App() {
         <Hero />
         <About />
         <Skills />
+        <Experience />
         <Projects />
         <Education />
         <Contact />
