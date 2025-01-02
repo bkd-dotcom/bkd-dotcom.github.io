f130682e77bc10e72528f64e9c054702f90b3372
Binay <150876063+bkd-dotcom@users.noreply.github.com>
2025-01-01 21:39:57 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
 import { Background } from './components/Background';
+import { Education } from './components/Education';
 
 export default function App() {
   return (
