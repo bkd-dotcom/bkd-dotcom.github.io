c6ec4b9a2cd69febaa836b8ee593d863421806c6
bkd-dotcom <bkd5497@psu.edu>
2025-07-20 12:17:01 -0400

    Revert to default Vite config for original deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bkd-dotcom.github.io/',
-  build: {
-    outDir: 'docs',
-  },
   plugins: [react()],
 });
